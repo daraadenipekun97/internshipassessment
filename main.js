c94fab6697b750d6f62d38a26aff1ddb1622327f
daraadenipekun97 <daraadenipekun97@gmail.com>
2021-05-20 07:42:06 +0100

    setting up event listener

--- main.js
+++ main.js
@@ -5,12 +5,54 @@ const getData = () =>{
     fetch("https://reqres.in/api/users")
     .then(res => res.json())
     .then(users => {
-        console.log(users.data);
+        console.log(users.data[0].id);
+       
         const render = users.data.map(user =>{
-            return `<div class = "name"><h3>${user.first_name}</h3></div>`
+                return `<div class = "details">
+                   
+                    <p><img src = "${user.avatar}" alt = "${user.first_name}"/></p>
+                     <h3 class = "name">${user.first_name} 
+                     
+                     <ul class = "list">
+                        <li class = "list_items">User ID: ${user.id}</li>
+                        <li class = "list_items">Lastname: ${user.last_name}</li>
+                        <li class = "list_items">Email: ${user.email}</li>
+                    </ul>
+                     
+                     </h3>
+                     
+                
+                </div>`
         }).join("")
-            console.log(render)
+            console.log(render);
+        
             document.getElementById("userdetails").innerHTML = render;
+
+
+            let nameEl = document.querySelectorAll(".name");
+            let listEl = document.querySelectorAll(".list");
+            console.log(listEl.length);
+            
+            for (let i = 0; i < nameEl.length; i++) {
+                nameEl[i].addEventListener("click", action);
+
+                
+                
+            }
+
+           
+
+                function action(e){
+                       this.style.color = "brown";
+                       this.firstElementChild.style.display = "block"
+                    
+
+                   
+                           
+                      
+
+                }
+            
     })
     .catch(error => {
         console.log(error) 
